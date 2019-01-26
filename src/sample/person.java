0d7cb28cf358efa8f909c3f95bcf66fa85794f09
Duncan Skilton <32618413+DSkilton@users.noreply.github.com>
2019-01-26 13:19:40 +0000

    Add files via upload
    add abstract person class with getters and setters.

---
+++ src/sample/person.java
@@ -0,0 +1,141 @@
+package sample;
+
+import java.util.Date;
+
+public abstract class person {
+    private int personId;
+    private String title;
+    private String firstName;
+    private String lastName;
+    private String addA;
+    private String addB;
+    private String town;
+    private String postcode;
+    private String country;
+    private String homeNumber;
+    private String mobNumber;
+    private String status;
+    private Date registered;
+    private Date lastContact;
+    private Date nextContact;
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddA() {
+        return addA;
+    }
+
+    public void setAddA(String addA) {
+        this.addA = addA;
+    }
+
+    public String getAddB() {
+        return addB;
+    }
+
+    public void setAddB(String addB) {
+        this.addB = addB;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHomeNumber() {
+        return homeNumber;
+    }
+
+    public void setHomeNumber(String homeNumber) {
+        this.homeNumber = homeNumber;
+    }
+
+    public String getMobNumber() {
+        return mobNumber;
+    }
+
+    public void setMobNumber(String mobNumber) {
+        this.mobNumber = mobNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
+    }
+
+    public Date getLastContact() {
+        return lastContact;
+    }
+
+    public void setLastContact(Date lastContact) {
+        this.lastContact = lastContact;
+    }
+
+    public Date getNextContact() {
+        return nextContact;
+    }
+
+    public void setNextContact(Date nextContact) {
+        this.nextContact = nextContact;
+    }
+}
