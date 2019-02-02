530cbf249a1df3019b81c1b81ea4b818e765eac6
Duncan Skilton <32618413+DSkilton@users.noreply.github.com>
2019-02-02 21:38:10 +0000

    Add files via upload
    login form is completed, functionality needs adding and checking

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,14 +8,13 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     Stage window;
-    Scene scene;
-    Button button;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,46 +23,68 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-        window.setTitle("Checkbox Demo");
-        Button button1 = new Button("Login");
-        Button button2 = new Button("Help");
+        window.setTitle("Login");
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram();
+        });
 
         //String doesn't need to be set at the second diamond because its emplied
         ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        //getItems returns the ObservableList object
         choiceBox.getItems().add("William Conniston");
 
-        //getItems returns the ObservableList object
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10,10,10,10));
         grid.setVgap(8);
         grid.setHgap(10);
+
+        HBox hbButtons = new HBox();
+        hbButtons.setSpacing(10.0);
 
         //Name Label
         Label nameLabel = new Label("Username");
         GridPane.setConstraints(nameLabel, 0, 0);
 
         //Name Input
-        TextField nameInput = new TextField("Username");
+        TextField nameInput = new TextField();
+        nameInput.setPromptText("username");
         GridPane.setConstraints(nameInput,1,0);
 
         //Password Label
         Label passLabel = new Label("Password");
-        GridPane.setConstraints(passLabel,1,1);
+        GridPane.setConstraints(passLabel,0,1);
 
         //Password Input
         TextField passInput = new TextField();
         passInput.setPromptText("password");
-        GridPane.setConstraints(passInput,1,2);
+        GridPane.setConstraints(passInput,1,1);
 
         Button loginButton = new Button ("Login");
-        GridPane.setConstraints(loginButton,1,2);
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setOnAction(e -> {
+            e.consume();
+            closeProgram();
+        });
+        Button helpButton = new Button("Help");
 
-        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton);
-        Scene scene = new Scene(grid, 300, 200);
+        GridPane.setConstraints(cancelButton,1,2);
+        GridPane.setConstraints(cancelButton,2,2);
+        GridPane.setConstraints(helpButton,3,2);
 
+        hbButtons.getChildren().addAll(loginButton, cancelButton, helpButton);
+        grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput);
+        grid.add(hbButtons, 0,2,2,1);
+        Scene scene = new Scene(grid, 450, 200);
+        window.setScene(scene);
         window.show();
     } // end of primaryStage
 
+    private void closeProgram(){
+//        Boolean answer = ConfirmBox.display("Exit Dialogue", "Are you sure you want to exit?");
+//            if(answer)
+                window.close();
+    }
 }
 // end of class
 
