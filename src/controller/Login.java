e3c85afcdb3d6d7a33d8a65f615291501d34979d
Duncan Skilton <32618413+DSkilton@users.noreply.github.com>
2019-02-17 16:43:54 +0000

    Add files via upload

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -1,18 +1,44 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import view.AlertBox;
 
 public class Login {
     @FXML private TextField username;
     @FXML private PasswordField password;
-    @FXML private Button login;
-    @FXML private Button cancel;
-    @FXML private Button help;
+    @FXML private Button loginButton;
+    @FXML private Button cancelButton;
+    @FXML private Button helpButton;
+
+    @FXML private javafx.scene.control.Button closeButton;
+//    @FXML private javafx.scene.control.Button cancelButton;
+//    @FXML private javafx.scene.control.Button helpButton;
+
+    @FXML
+    private void loginButtonAction(){
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
+
+    @FXML
+    private void closeButtonAction(){
+        closeButton.setOnAction(actionEvent -> Platform.exit());
+    }
+
+    @FXML
+    private void helpButtonAction(){
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
 
     //Static details for testing. Password would be converted to SHA-256 and stored in a database for future itterations
     public static final String USERNAME = "admin";
@@ -30,8 +56,7 @@ public class Login {
         } catch (Exception e) {
             e.getStackTrace();
         }
+    } //end of login check
 
 
-
-    }
 }
