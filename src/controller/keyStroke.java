6a3a363e48fb692122d65584387d8e402c2fedf9
Duncan Skilton <32618413+DSkilton@users.noreply.github.com>
2019-02-25 14:39:42 +0000

    Add files via upload
    Attemping KeyStoke event

---
+++ src/controller/keyStroke.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javax.swing.*;
+
+public class keyStroke {
+
+    public static void getKeyStroke() {
+        KeyStroke t = KeyStroke.getKeyStroke("T");
+        KeyStroke controltT = KeyStroke.getKeyStroke("Alt T");
+
+    }
+}
