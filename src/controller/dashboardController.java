45b1be827cc9dfef7e011f9a311c40edde470267
Duncan Skilton <32618413+DSkilton@users.noreply.github.com>
2019-02-26 14:22:04 +0000

    Add files via upload
    trying to figure out InputMap and ActionMap

--- src/controller/dashboardController.java
+++ src/controller/dashboardController.java
@@ -1,15 +1,28 @@
 package controller;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import jdk.internal.util.xml.impl.Input;
 
-import java.io.IOException;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class dashboardController {
 
-    public void dashloader() {
+    public void getKeyStroke() {
+        KeyStroke CtrlT = KeyStroke.getKeyStroke("Control + T");
+        InputMap inputMap = CtrlT.getInputMap();
+        inputMap.put(KeyStroke.getKeyStroke("CtrlT"), "CtrlT");
+        ACtionMap actionMap = CtrlT.getActionMap();
+        actionMap.put("CtrlT", actionListener);
+
+        Action actionListener= new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                public void actionPerformed(ActionEvent actionEvent){
+
+                }
+            }
+        }
 
     }
 }
